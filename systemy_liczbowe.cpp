cad61f483714dd4c331ad6d873070656a3a0187f
nopcio <157692309+nopcio@users.noreply.github.com>
2024-01-25 17:33:37 +0100

    systemy_liczbowe.cpp

--- systemy_liczbowe.cpp
+++ systemy_liczbowe.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-string dec2p(int liczba, int podstawa){
+string sl(int liczba, int podstawa){
     string wynik="";
     int reszta;
     if(liczba==0) wynik="0";
@@ -13,15 +13,15 @@ string dec2p(int liczba, int podstawa){
         liczba=liczba/podstawa;
     }
 
-    return wynik;
+    return 0;
 }
 
 int main() {
-    int liczbaDec;
+    int liczbadziesietna;
     cout << "podaj liczbe w systemie dziesietnym: ";
-    cin >> liczbaDec;
-    cout << "system dwojkowy: " << dec2p (liczbaDec,2) <<endl;
-    cout << "system osemkowy: " << dec2p (liczbaDec,8) << endl;
-    cout << "system szesnastkowy: " << dec2p(liczbaDec, 16) << endl;
+    cin >> liczbadziesietna;
+    cout << "system dwojkowy: " << sl (liczbadziesietna,2) <<endl;
+    cout << "system osemkowy: " << sl (liczbadziesietna,8) << endl;
+    cout << "system szesnastkowy: " << sl(liczbadziesietna, 16) << endl;
     return 0;
 }
